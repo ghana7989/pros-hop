846d247ad44d05c3b8fa4393beea5a4c28d4779f
ghana <ghana.chindukuri@gmail.com>
2020-11-27 12:20:36 +0530

    Heroku Setup

--- backend/server.js
+++ backend/server.js
@@ -23,19 +23,20 @@ if (process.env.NODE_ENV === "development") {
 }
 app.use(express.json())
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, "/uploads")))
-
-app.get("/", (req, res) => {
-  res.send("API is working")
-})
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, "/uploads")))
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/front-end/build")))
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, 'front-end', 'build', 'index.html')))
+}
 
 app.use(notFound)
 
